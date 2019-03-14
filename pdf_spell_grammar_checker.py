9f170e8bb683d332f561bc5f46d9a373ae3cd4a5
Pratik Anurag <38415712+pratikanurag@users.noreply.github.com>
2019-03-14 15:04:40 +0530

    Update pdf_spell_grammar_checker.py

--- pdf_spell_grammar_checker.py
+++ pdf_spell_grammar_checker.py
@@ -1,12 +1,13 @@
 import PyPDF2
 import enchant
-import grammar_check
-tool = grammar_check.LanguageTool('en-GB')
+import language_check
+tool = language_check.LanguageTool('en-US')
 d =     enchant.Dict("en_US")
 pdfFileObj = open('test.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 pageObj = pdfReader.getPage(0)
 text = pageObj.extractText()
+print(text)
 for i in text.split():
     # print(i)
     d.check(i)
@@ -17,4 +18,5 @@ matches = tool.check(text)
 # print((matches))
 print('Starting with grammar check...')
 print('The suggested sentence is:-')
-print(grammar_check.correct(text, matches))
+print(language_check.correct(text, matches))
+
